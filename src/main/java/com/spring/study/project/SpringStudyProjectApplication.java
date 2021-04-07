195ba5a6ef6e10199eeb5169b3af74d038fa7789
wellbell <uok02018496@naver.com>
2021-04-08 08:49:21 +0900

    Initial commit

---
+++ src/main/java/com/spring/study/project/SpringStudyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.spring.study.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStudyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStudyProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/study/project/SpringStudyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.study.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStudyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
